375bf7d3c4b7c4346334dc9f2471a6954f453592
David Golembiowski <dmgolembiowski@gmail.com>
2019-06-13 01:13:09 -0400

    Need to define __getitem__ and __setitem__ behavior such that every time we assign a key -- like 'A3' -- to s, where s = Sheet(), it will assign s['A3'] = s['a3'] = Value('a3'). Furthermore, Value('a3') must cache that the key has been used by adding the lower string combination to a set. It will test if eval('A3'.lower()) is not already in Value.addresses; if it is then the function call will just return the Value('a3') instance.

---
+++ backend/aioexcelpy/table.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3.7
+from __future__ import annotations
+
+class Value:
+    def __init__(
+            self,
+            Id=''
+            Val=None,
+            Input='',
+            Row=0,
+            Col='',
+            **kwargs):
+        
+        self._internal = {
+                'val': Val,
+                'input': Input,
+                'row': Row,
+                'col': Col,
+                'id': Id}
+        
+        self._val = self._internal['val']
+        self._input = self._internal['input']
+        self._row = self._internal['row']
+        self._col = self._internal['col']
+        self._id = self._internal['id']
+        self.__dict__.update(kwargs)
+
+    @property
+    def val(self):
+        return self._val
+
+    @val.setter
+    def val(self, Val):
+        self._internal['val'] = Val
+
+    @property
+    def input(self):
+        return self._input
+
+    @input.setter
+    def input(self, Input):
+        self._internal['input'] = Input
+
+    @property
+    def row(self):
+        return self._row
+
+    @row.setter
+    def row(self, Row):
+        self._internal['row'] = Row
+
+    @property
+    def col(self):
+        return self._col
+
+    @col.setter
+    def col(self, Col):
+        self._internal['col'] = Col
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, Id):
+        self._internal['id'] = Id
+
+    def __un__(self):
+        return self._internal
+
+    @staticmethod
+    def __nu__(attrs: dict):
+        return Value.__init__(**attrs)
+
+    def __repr__(self):
+        return str(self._internal)
+
+class Sheet(dict):
+
+    def __init__(self):
+        super().__init__()
+    
+    def __setitem__(self):
+
+
+class Mapper:
+    def __init__(self):
+        self._sheet = {}
+
+    @property
+    def sheet(self):
+        return self._sheet
+
+    
